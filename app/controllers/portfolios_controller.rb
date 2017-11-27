080a5fc0b5cd6dbf686b4085787969cdb11706c0
Jorge Regidor <jorgeregidor@gmail.com>
2017-11-27 22:12:14 +0000

    refactored porfolio controller

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -10,7 +10,7 @@ class PortfoliosController < ApplicationController
 	end
 
 	def create
-	    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+	    @portfolio_item = Portfolio.new(portfolio_params)
 
 	    respond_to do |format|
 	      if @portfolio_item.save
@@ -29,7 +29,7 @@ class PortfoliosController < ApplicationController
   		@portfolio_item = Portfolio.find(params[:id])
 
 	    respond_to do |format|
-	      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+	      if @portfolio_item.update(portfolio_params)
 	        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
 	      else
 	        format.html { render :edit }
@@ -47,6 +47,12 @@ class PortfoliosController < ApplicationController
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Portfolio was successfully destroyed.' }
     end
+  end
+
+  private 
+
+  def portfolio_params
+  	params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])
   end
 
 end
